fe63a570217db6e13559875cc98237e83355665d
ysereda <sereda_yu@yahoo.com>
2021-05-26 10:51:43 -0400

    cycle upto 7 cities

--- Traveling_Salesman.py
+++ Traveling_Salesman.py
@@ -1,62 +1,58 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[291]:
+# In[433]:
 
 
 # City names alphabetically
-cityNames = ['Винница','Днепр','Житомир','Запорожье','Ивано-Франковск','Киев','Кропивницкий','Луцк','Львов']
-#,'Николаев','Одесса','Полтава','Ровно','Севастополь','Симферополь','Сумы','Тернополь','Ужгород','Харьков','Херсон','Хмельницкий','Черкассы','Чернигов','Черновцы'
+cityNames = ['Винница','Днепр','Житомир','Запорожье','Ивано-Франковск','Киев','Кропивницкий','Луцк','Львов','Николаев']
+#,'Одесса','Полтава','Ровно','Севастополь','Симферополь','Сумы','Тернополь','Ужгород','Харьков','Херсон','Хмельницкий','Черкассы','Чернигов','Черновцы'
 # 'Донецк','Луганск',
 print(cityNames)
-N = len(cityNames)
-print("Number of cities: ",N)
+N = len(cityNames); print("Number of cities: ",N)
 
 
-# In[292]:
+# In[445]:
 
 
 # Distance matrix using maps.google.com. Last included city is 'Луцк'
 d = [
-    [  0, 588, 128, 651, 368, 268, 322, 389, 364],
-    [588,   0, 596,85.4, 962, 480, 247, 875, 957],
-    [128, 596,   0, 690, 413, 140, 392, 261, 402],
-    [651,85.4, 690,   0,1026, 538, 310, 952,1021],
-    [368, 962, 413,1026,   0, 560, 694, 263, 132],
-    [268, 480, 140, 538, 560,   0, 303, 400, 541],
-    [322, 247, 392, 310, 694, 303,   0, 670, 693],
-    [389, 875, 261, 952, 263, 400, 670,   0, 180],
-    [364, 957, 402,1021, 132, 541, 693, 180,   0]
+    [  0, 588, 128, 651, 368, 268, 322, 389, 364, 431],
+    [588,   0, 596,85.4, 962, 480, 247, 875, 957, 322],
+    [128, 596,   0, 690, 413, 140, 392, 261, 402, 507],
+    [651,85.4, 690,   0,1026, 538, 310, 952,1021, 376],
+    [368, 962, 413,1026,   0, 560, 694, 263, 132, 780],
+    [268, 480, 140, 538, 560,   0, 303, 400, 541, 481],
+    [322, 247, 392, 310, 694, 303,   0, 670, 693, 182],
+    [389, 875, 261, 952, 263, 400, 670,   0, 180, 779],
+    [364, 957, 402,1021, 132, 541, 693, 180,   0, 802],
+    [431, 322, 507, 376, 780, 481, 182, 779, 802,   0]
     ]
-#print(d)
+d = np.array(d)
 
 # Time by car
 t = [
-    [  0, 484,  99, 521, 336, 236, 266, 311, 323],
-    [484,   0, 474,  82, 866, 407, 234, 706, 765],
-    [ 99, 474,   0, 582, 396, 101, 352, 204, 304],
-    [521,  82, 582,   0, 898, 483, 271, 784, 826],
-    [336, 866, 396, 898,   0, 482, 623, 259, 124],
-    [236, 407, 101, 483, 482,   0, 263, 309, 383],
-    [266, 234, 352, 271, 623, 263,   0, 589, 581],
-    [311, 706, 204, 784, 259, 309, 589,   0, 166],
-    [323, 765, 304, 826, 124, 383, 581, 166,   0]
+    [  0, 484,  99, 521, 336, 236, 266, 311, 323, 357],
+    [484,   0, 474,  82, 866, 407, 234, 706, 765, 274],
+    [ 99, 474,   0, 582, 396, 101, 352, 204, 304, 419],
+    [521,  82, 582,   0, 898, 483, 271, 784, 826, 305],
+    [336, 866, 396, 898,   0, 482, 623, 259, 124, 692],
+    [236, 407, 101, 483, 482,   0, 263, 309, 383, 377],
+    [266, 234, 352, 271, 623, 263,   0, 589, 581, 141],
+    [311, 706, 204, 784, 259, 309, 589,   0, 166, 632],
+    [323, 765, 304, 826, 124, 383, 581, 166,   0, 663],
+    [357, 274, 419, 305, 692, 377, 141, 632, 663,   0]
     ]
-#print(t)
+t = np.array(t)
 
 
-# In[293]:
+# In[452]:
 
 
 def PR(): # print result
     global r,d_min
     print("Shortest route:",r)
     print("Shortest distance:",d_min)
-
-
-# In[295]:
-
-
 def init():
     global d_min,r
     d_min=0 # current distance
@@ -66,12 +62,7 @@ def init():
         r.append(i)
     d_min += d[0][N-1]
     r.append(0)
-    PR()    
-
-
-# In[296]:
-
-
+    PR()
 def opt2():
     print("Optimize pairs")
     global d_min,r
@@ -89,13 +80,8 @@ def opt2():
                 d_min -= l1-l2
                 print(A,B,'->',B,A)
         if d_min < d_old: PR()
-
-
-# In[297]:
-
-
 def opt3():
-    print("Optimize triples")
+    print("Cycle 3 cities")
     global d_min,r
     d_old = d_min+1
     while d_min < d_old:
@@ -130,13 +116,8 @@ def opt3():
                 print('Needed!')
                 print(A,B,C,'->',C,B,A)
         if d_min < d_old: PR()
-
-
-# In[298]:
-
-
 def opt4():
-    print("Optimize 4 cities (cyclically)")
+    print("Cycle 4 cities")
     global d_min,r
     d_old = d_min+1
     while d_min < d_old:
@@ -168,13 +149,8 @@ def opt4():
                 A = r[i]; B = r[i+1]; C = r[i+2]; D = r[i+3];
                 l1 = d[SC][A] + d[A][B] + d[B][C] + d[C][D] + d[D][FC]
         if d_min < d_old: PR()
-
-
-# In[299]:
-
-
 def opt5():
-    print("Optimize 5 cities (cyclically)")
+    print("Cycle 5 cities")
     global d_min,r
     d_old = d_min+1
     while d_min < d_old:
@@ -213,13 +189,8 @@ def opt5():
                 l1 = l2
                 A = r[i+1]; B = r[i+2]; C = r[i+3]; D = r[i+4]; E = r[i+5];           
         if d_min < d_old: PR()
-
-
-# In[300]:
-
-
 def opt6():
-    print("Optimize 6 cities (cyclically)")
+    print("Cycle 6 cities")
     global d_min,r
     d_old = d_min+1
     while d_min < d_old:
@@ -265,83 +236,77 @@ def opt6():
                 l1 = l2
                 A = r[i+1]; B = r[i+2]; C = r[i+3]; D = r[i+4]; E = r[i+5]; F = r[i+6];           
         if d_min < d_old: PR()
+def opt7():
+    print("Cycle 7 cities")
+    global d_min,r
+    d_old = d_min+1
+    while d_min < d_old:
+        d_old = d_min
+        for i in range(N-7):
+            SC = r[i]; # starting city, fixed
+            A = r[i+1]; B = r[i+2]; C = r[i+3]; D = r[i+4]; E = r[i+5]; F = r[i+6]; G = r[i+7]; # next 7 cities
+            FC = r[i+8]; # finishing city, fixed
+            l1 = d[SC][A] + d[A][B] + d[B][C] + d[C][D] + d[D][E] + d[E][F] + d[F][G] + d[G][FC] # ABCDEFG
+            l2 = d[SC][B] + d[B][C] + d[C][D] + d[D][E] + d[E][F] + d[F][G] + d[G][A] + d[A][FC] # BCDEFGA
+            if l2 < l1:
+                print(A,B,C,D,E,F,G,'->',B,C,D,E,F,G,A)
+                r[i+1] = B; r[i+2] = C; r[i+3] = D; r[i+4] = E; r[i+5] = F; r[i+6] = G; r[i+7] = A; # ABCDEFG -> BCDEFGA
+                d_min -= l1-l2
+                l1 = l2
+                A = r[i+1]; B = r[i+2]; C = r[i+3]; D = r[i+4]; E = r[i+5]; F = r[i+6]; G = r[i+7];
+            l2 = d[SC][C] + d[C][D] + d[D][E] + d[E][F] + d[F][G] + d[G][A] + d[A][B] + d[B][FC] # CDEFGAB
+            if l2 < l1:
+                print(A,B,C,D,E,F,G,'->',C,D,E,F,G,A,B)
+                r[i+1] = C; r[i+2] = D; r[i+3] = E; r[i+4] = F; r[i+5] = G; r[i+6] = A; r[i+7] = B; # ABCDEFG -> CDEFABG
+                d_min -= l1-l2
+                l1 = l2
+                A = r[i+1]; B = r[i+2]; C = r[i+3]; D = r[i+4]; E = r[i+5]; F = r[i+6]; G = r[i+7];
+            l2 = d[SC][D] + d[D][E] + d[E][F] + d[F][G] + d[G][A] + d[A][B] + d[B][C] + d[C][FC] # DEFGABC
+            if l2 < l1:
+                print(A,B,C,D,E,F,G,'->',D,E,F,G,A,B,C)
+                r[i+1] = D; r[i+2] = E; r[i+3] = F; r[i+4] = G; r[i+5] = A; r[i+6] = B; r[i+7] = C; # ABCDEFG -> DEFGABC
+                d_min -= l1-l2
+                l1 = l2
+                A = r[i+1]; B = r[i+2]; C = r[i+3]; D = r[i+4]; E = r[i+5]; F = r[i+6]; G = r[i+7];           
+            l2 = d[SC][E] + d[E][F] + d[F][G] + d[G][A] + d[A][B] + d[B][C] + d[C][D] + d[D][FC] # EFGABCD
+            if l2 < l1:
+                print(A,B,C,D,E,F,G,'->',E,F,G,A,B,C,D)
+                r[i+1] = E; r[i+2] = F; r[i+3] = G; r[i+4] = A; r[i+5] = B; r[i+6] = C; r[i+7] = D; # ABCDEFG -> EFGABCD
+                d_min -= l1-l2
+                l1 = l2
+            A = r[i+1]; B = r[i+2]; C = r[i+3]; D = r[i+4]; E = r[i+5]; F = r[i+6]; G = r[i+7];          
+            l2 = d[SC][F] + d[F][G] + d[G][A] + d[A][B] + d[B][C] + d[C][D] + d[D][E] + d[E][FC] # FGABCDE
+            if l2 < l1:
+                print(A,B,C,D,E,F,G,'->',F,G,A,B,C,D,E)
+                r[i+1] = F; r[i+2] = G; r[i+3] = A; r[i+4] = B; r[i+5] = C; r[i+6] = D; r[i+7] = E; # ABCDEFG -> FGABCDE
+                d_min -= l1-l2
+                l1 = l2
+                A = r[i+1]; B = r[i+2]; C = r[i+3]; D = r[i+4]; E = r[i+5]; F = r[i+6]; G = r[i+7];           
+            l2 = d[SC][G] + d[G][A] + d[A][B] + d[B][C] + d[C][D] + d[D][E] + d[E][F] + d[F][FC] # GABCDEF
+            if l2 < l1:
+                print(A,B,C,D,E,F,G,'->',G,A,B,C,D,E,F)
+                r[i+1] = G; r[i+2] = A; r[i+3] = B; r[i+4] = C; r[i+5] = D; r[i+6] = E; r[i+7] = F;  # ABCDEFG -> GABCDEF
+                d_min -= l1-l2
+                l1 = l2
+                A = r[i+1]; B = r[i+2]; C = r[i+3]; D = r[i+4]; E = r[i+5]; F = r[i+6]; G = r[i+7];           
+        if d_min < d_old: PR()
 
 
-# In[301]:
+# In[453]:
 
 
-init()
+init(); opt2(); opt3(); opt4(); opt5(); opt6(); opt7()
 
 
-# In[302]:
+# In[454]:
 
 
-opt2()
-
-
-# In[303]:
-
-
-opt3()
-
-
-# In[304]:
-
-
-opt4()
-
-
-# In[305]:
-
-
-opt5()
-
-
-# In[306]:
-
-
-opt6()
-
-
-# In[307]:
-
-
-opt5()
-
-
-# In[308]:
-
-
-opt4()
-
-
-# In[309]:
-
-
-opt3()
-
-
-# In[314]:
-
-
-opt2()
-
-
-# In[315]:
-
-
-d_min
-
-
-# In[ ]:
-
-
-
+opt6(); opt5(); opt4(); opt3(); opt2();
 
 
 # ## Results
 
-# In[316]:
+# In[443]:
 
 
 print("Current shortest route: ",d_min,"\nStart\tEnd\tDist")
@@ -349,7 +314,7 @@ for i in range(len(r)-1):
     print(cityNames[r[i]],"\t",cityNames[r[i+1]],"\t",d[r[i]][r[i+1]])
 
 
-# In[317]:
+# In[444]:
 
 
 # Visualize the shortest route
@@ -357,8 +322,8 @@ import numpy as np
 from matplotlib import pyplot as plt
 import pandas as pd
 # customer locations, geographical coordinates in degrees
-latitude = np.array([49.2347128, 48.4622135, 50.2678654, 47.8561438, 48.9117518, 50.401699, 48.5187443, 50.73977, 49.8326679])
-longitude = np.array([28.3995942, 34.8602731, 28.6036778, 35.0352701, 24.6470892, 30.2525101, 32.1456232, 25.2639651, 23.9421958])
+latitude = np.array([49.2347128, 48.4622135, 50.2678654, 47.8561438, 48.9117518, 50.401699, 48.5187443, 50.73977, 49.8326679, 46.9329791])
+longitude = np.array([28.3995942, 34.8602731, 28.6036778, 35.0352701, 24.6470892, 30.2525101, 32.1456232, 25.2639651, 23.9421958, 31.8679134])
 # convert to radians
 Pi = np.pi
 #latitude = Pi/180*latitude
